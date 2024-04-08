f374754be7b3e8bd7d41b542060916533c5f6a33
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-08 11:03:20 +0200

    [add] global css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
+import 'tailwindcss/tailwind.css';
+import '/styles/globals.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
 	return (
