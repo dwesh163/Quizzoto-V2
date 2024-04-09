2321d8c9ce0c0eed839bf1d5a3c223bef1a26561
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-09 07:36:03 +0200

    [add] Insert a new user in MongoDb

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -12,6 +12,39 @@ export const authOptions = {
 		signIn: '/auth/signin',
 		error: '/auth/error',
 	},
+	callbacks: {
+		async signIn({ user, account, profile }) {
+			try {
+				const existingUser = await db.collection('users').findOne({ email: user.email });
+
+				const newUser = {
+					id: uuidv4(),
+					email: user.email,
+					username: profile.login,
+					image: user.image,
+					provider: account.provider,
+					company: profile.company,
+					name: profile.name ? profile.name : profile.login,
+				};
+
+				if (existingUser) {
+					await db.collection('users').updateOne({ email: user.email }, { $set: { ...newUser, id: existingUser.id } });
+				} else {
+					await db.collection('users').insertOne(newUser);
+				}
+				return Promise.resolve(true);
+			} catch (error) {
+				console.error('Error during sign-in:', error);
+				return Promise.resolve(false);
+			}
+		},
+
+		async session({ session, token }) {
+			const user = await db.collection('users').findOne({ email: session.user.email });
+			session.user = user;
+			return session;
+		},
+	},
 };
 
 export default NextAuth(authOptions);
