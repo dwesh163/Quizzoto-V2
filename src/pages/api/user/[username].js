58e117a516b73b8fadfde364aeab9defce13d119
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-09 13:49:35 +0200

    [add] a try catch

--- src/pages/api/user/[username].js
+++ src/pages/api/user/[username].js
@@ -1,18 +1,23 @@
 import db from '/lib/mongodb';
 
 export default async function getUserInfo(req, res) {
-	const user = await db.collection('users').findOne(
-		{ username: req.query.username },
-		{
-			projection: {
-				_id: 0,
-				username: 1,
-				image: 1,
-				name: 1,
-				statistics: 1,
-			},
-		}
-	);
+	try {
+		const user = await db.collection('users').findOne(
+			{ username: req.query.username },
+			{
+				projection: {
+					_id: 0,
+					username: 1,
+					image: 1,
+					name: 1,
+					statistics: 1,
+				},
+			}
+		);
 
-	return res.status(200).send(user);
+		return res.status(200).send(user);
+	} catch (error) {
+		console.error('Error:', error);
+		return res.status(500).send('Internal server error');
+	}
 }
