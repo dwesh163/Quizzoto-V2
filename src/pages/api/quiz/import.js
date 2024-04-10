ae6ea46679796d6ceb7803789ca16f234ebbdf91
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-10 07:36:45 +0200

    [add] Mongo database and import function

--- src/pages/api/quiz/import.js
+++ src/pages/api/quiz/import.js
@@ -1,5 +1,7 @@
 import multer from 'multer';
 import { getSession } from 'next-auth/react';
+import db from '/lib/mongodb';
+import { v4 as uuidv4 } from 'uuid';
 
 const upload = multer();
 
@@ -11,9 +13,42 @@ export const config = {
 
 const uploadMiddleware = upload.single('files');
 
+async function insertQuiz(quiz, session) {
+	if (!session.user.id) {
+		return { code: 401, message: "You don't seem to be connected..." };
+	}
+
+	if (quiz.creator != session.user.username) {
+		return { code: 403, message: "The creator listed in the JSON doesn't seem to be you..." };
+	}
+
+	if (!quiz.slug || !quiz.title || !quiz.description || !quiz.image || !quiz.info || !quiz.creator || !quiz.questions) {
+		return { code: 400, message: 'Apparently not everything is here...' };
+	}
+
+	const existingSlug = await db.collection('quizzes').findOne({ slug: quiz.slug });
+
+	if (existingSlug) {
+		return { code: 400, message: 'The slug must be unique...' };
+	}
+
+	const newQuiz = {
+		id: uuidv4(),
+		slug: quiz.slug,
+		title: quiz.title,
+		description: quiz.description,
+		image: quiz.image,
+		info: quiz.info,
+		creator: session.user.id,
+		questions: quiz.questions,
+	};
+
+	await db.collection('quizzes').insertOne(newQuiz);
+	return { code: 200, message: 'Import success' };
+}
+
 export default async function handler(req, res) {
 	const session = await getSession({ req });
-	console.log(session);
 	try {
 		await new Promise((resolve, reject) => {
 			uploadMiddleware(req, res, (err) => {
@@ -26,14 +61,15 @@ export default async function handler(req, res) {
 
 		if (file && file.mimetype === 'application/json') {
 			const jsonData = JSON.parse(file.buffer.toString());
+			const response = await insertQuiz(jsonData, session);
 			setTimeout(() => {
-				res.status(200).send('ok');
-			}, 5000);
+				res.status(response.code).send(response);
+			}, 3000);
 		} else {
-			res.status(400).json({ message: 'Veuillez envoyer un fichier JSON.' });
+			res.status(400).json({ message: "Please send a JSON file, as this doesn't appear to be the case..." });
 		}
 	} catch (error) {
 		console.error('Error:', error);
-		res.status(500).json({ message: 'Une erreur est survenue lors du traitement du fichier.' });
+		res.status(500).json({ message: 'An error has occurred while processing the file...' });
 	}
 }
