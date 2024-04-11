e50403f6d5a67bb81f869710c2b62738794fd340
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-11 08:16:38 +0200

    [add] Page for displaying quiz information from the API

---
+++ src/pages/quiz/[slug]/start.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import Header from '@/components/header';
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>QuizzotoV2</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main>
+				<Header />
+				<h1>Start</h1>
+			</main>
+		</>
+	);
+}
