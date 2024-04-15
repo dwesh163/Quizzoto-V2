7b7dd2e46bbea21d70b6677d5c7f5b628e63cedd
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-15 08:40:56 +0200

    [add] API to get a question with the ID

---
+++ src/pages/api/quiz/[slug]/[questionId].js
@@ -0,0 +1,26 @@
+import db from '/lib/mongodb';
+
+export default async function getQuestionInfo(req, res) {
+	const questionId = req.query.questionId - 1;
+	try {
+		let quiz = await db.collection('quizzes').findOne(
+			{ slug: req.query.slug },
+			{
+				projection: {
+					_id: 0,
+					questions: 1,
+				},
+			}
+		);
+
+		if (quiz == null || quiz.questions[questionId] == null) {
+			return res.status(200).send('404');
+		}
+
+		delete quiz.questions[questionId].correct;
+		return res.status(200).send(quiz.questions[questionId]);
+	} catch (error) {
+		console.error('Error:', error);
+		return res.status(500).send('Internal server error');
+	}
+}
