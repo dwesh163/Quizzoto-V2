728426c9b98b128a00d1a49d06ae245f1b2b1139
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-26 09:31:57 +0200

    [add] Get data from a specified room

---
+++ src/pages/api/room/[roomId].js
@@ -0,0 +1,69 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../auth/[...nextauth]';
+import db from '/lib/mongodb';
+
+export default async function getRoomsInfo(req, res) {
+	const session = await getServerSession(req, res, authOptions);
+	if (!session) {
+		return res.status(200).send({ error: 'Not Found' });
+	}
+	try {
+		const results = await db
+			.collection('results')
+			.aggregate([
+				{
+					$match: {
+						roomId: req.query.roomId,
+					},
+				},
+				{
+					$lookup: {
+						from: 'users',
+						localField: 'player',
+						foreignField: 'id',
+						as: 'user',
+					},
+				},
+				{
+					$unwind: {
+						path: '$user',
+						preserveNullAndEmptyArrays: true,
+					},
+				},
+				{
+					$lookup: {
+						from: 'quizzes',
+						localField: 'quiz',
+						foreignField: 'id',
+						as: 'quiz',
+					},
+				},
+				{
+					$unwind: {
+						path: '$quiz',
+						preserveNullAndEmptyArrays: true,
+					},
+				},
+				{
+					$project: {
+						_id: 0,
+						points: 1,
+						roomId: 1,
+						visibility: 1,
+						'user.name': 1,
+						'user.username': 1,
+						'user.image': 1,
+						'quiz.title': 1,
+						'quiz.id': 1,
+						'quiz.description': 1,
+					},
+				},
+			])
+			.toArray();
+
+		return res.status(200).send({ results });
+	} catch (error) {
+		console.error('Error:', error);
+		return res.status(500).send('Internal server error');
+	}
+}
