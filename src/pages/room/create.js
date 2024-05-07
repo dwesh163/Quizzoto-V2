02f9f20e8e2bd13c2db54b636ff475c83a22c2c4
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-07 14:46:37 +0200

    [Add] slug and url inputs to create room form

--- src/pages/room/create.js
+++ src/pages/room/create.js
@@ -11,11 +11,13 @@ export default function Results() {
 
 	const [title, setTitle] = useState('');
 	const [comment, setComment] = useState('');
+	const [slug, setSlug] = useState('');
 	const [quizzes, setQuizzes] = useState([{ title: 'test' }]);
+	const [url, setUrl] = useState('');
 	const [error, setError] = useState('');
 
 	function createRoom(params) {
-		fetch('/api/room/', { method: 'POST', body: JSON.stringify({ title: title, comment: comment }) })
+		fetch('/api/room/', { method: 'POST', body: JSON.stringify({ title: title, comment: comment, slug: slug }) })
 			.then((response) => response.json())
 			.then((jsonData) => {
 				if (!jsonData.error) {
@@ -25,6 +27,12 @@ export default function Results() {
 				}
 			});
 	}
+
+	useEffect(() => {
+		if (window && router) {
+			setUrl(window.location.href.replace(router.pathname, ''));
+		}
+	}, [router]);
 
 	return (
 		<>
@@ -45,6 +53,23 @@ export default function Results() {
 						<div className="relative w-full min-w-[200px] h-10">
 							<input type="text" value={comment} onChange={(e) => setComment(e.target.value)} className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-blue-500" placeholder=" " />
 							<label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-blue-gray-400 peer-focus:text-blue-500 before:border-blue-gray-200 peer-focus:before:!border-blue-500 after:border-blue-gray-200 peer-focus:after:!border-blue-500">Room comment</label>
+						</div>
+					</div>
+					<div className="w-full flex mt-3">
+						<div className="w-52 flex items-center">
+							<p className="w-full">{url}/r/</p>
+						</div>
+
+						<div className="relative w-full min-w-[200px] h-10">
+							<input type="text" value={slug} onChange={(e) => setSlug(e.target.value)} className={'peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 ' + (error ? 'focus:border-red-500 border-red-500' : 'focus:border-blue-500')} placeholder=" " />
+							{error && (
+								<p class="flex items-center gap-1 mt-2 font-sans text-sm antialiased font-normal leading-normal text-red-500">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#ef4468" class="w-4 h-4 -mt-px">
+										<path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd"></path>
+									</svg>
+									{error}
+								</p>
+							)}
 						</div>
 					</div>
 					<h3 className="text-3xl mt-3 font-bold text-black text-left w-full">Quizzes</h3>
