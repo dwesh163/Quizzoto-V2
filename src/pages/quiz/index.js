b86df6931c7461ac4c9872aa0cc4d6047e7d945d
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-13 10:39:08 +0200

    [fix] if no quiz in search

--- src/pages/quiz/index.js
+++ src/pages/quiz/index.js
@@ -49,59 +49,60 @@ export default function User() {
 
 	const userList = (
 		<div className="divide-y mt-3 px-4 divide-gray-200 w-full flex flex-wrap justify-between items-center">
-			{quizzes
-				?.map((quiz, index) => (
-					<div onClick={() => router.push('/quiz/' + quiz.slug)} key={index + '-quiz-list'} className="mb-5 w-full h-full cursor-pointer rounded overflow-hidden shadow-lg">
-						<img className="w-full" src={quiz.image} alt={quiz.title} />
-						<div className="px-6 py-4">
-							<div className="flex justify-between mb-2">
-								<div className="font-bold text-xl">{quiz.title}</div>
-								{quiz.rating != 0 && (
-									<div className="flex items-center">
-										{Array.from({ length: 5 }).map((_, ratingIndex) =>
-											ratingIndex + 1 > Math.round(quiz.rating) ? (
-												<svg key={ratingIndex + '-rating-' + index} className="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-													<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-												</svg>
-											) : (
-												<svg key={ratingIndex + '-rating-' + index} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-													<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-												</svg>
-											)
-										)}
-									</div>
-								)}
+			{quizzes != 'none' &&
+				quizzes
+					?.map((quiz, index) => (
+						<div onClick={() => router.push('/quiz/' + quiz.slug)} key={index + '-quiz-list'} className="mb-5 w-full h-full cursor-pointer rounded overflow-hidden shadow-lg">
+							<img className="w-full" src={quiz.image} alt={quiz.title} />
+							<div className="px-6 py-4">
+								<div className="flex justify-between mb-2">
+									<div className="font-bold text-xl">{quiz.title}</div>
+									{quiz.rating != 0 && (
+										<div className="flex items-center">
+											{Array.from({ length: 5 }).map((_, ratingIndex) =>
+												ratingIndex + 1 > Math.round(quiz.rating) ? (
+													<svg key={ratingIndex + '-rating-' + index} className="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+														<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+													</svg>
+												) : (
+													<svg key={ratingIndex + '-rating-' + index} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+														<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+													</svg>
+												)
+											)}
+										</div>
+									)}
+								</div>
+								<p className="text-gray-700 text-base">{quiz.description}</p>
 							</div>
-							<p className="text-gray-700 text-base">{quiz.description}</p>
+							<div className="px-6 pt-4 pb-2">
+								{quiz?.tags?.map((tag, index) => (
+									<span key={index + '-tag-list'} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+										#{tag}
+									</span>
+								))}
+							</div>
 						</div>
-						<div className="px-6 pt-4 pb-2">
-							{quiz?.tags?.map((tag, index) => (
-								<span key={index + '-tag-list'} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-									#{tag}
-								</span>
+					))
+					.reduce((rows, div, index) => {
+						const columnCount = window.innerWidth >= 640 ? 3 : 1;
+						if (index % columnCount === 0) rows.push([]);
+						rows[rows.length - 1].push(div);
+						return rows;
+					}, [])
+					.map((row, rowIndex) => (
+						<div key={rowIndex} className="flex justify-between w-full border-none">
+							{row.map((col, colIndex) => (
+								<div key={colIndex} className="sm:w-[32%] w-full mb-5">
+									{col}
+								</div>
 							))}
 						</div>
-					</div>
-				))
-				.reduce((rows, div, index) => {
-					const columnCount = window.innerWidth >= 640 ? 3 : 1;
-					if (index % columnCount === 0) rows.push([]);
-					rows[rows.length - 1].push(div);
-					return rows;
-				}, [])
-				.map((row, rowIndex) => (
-					<div key={rowIndex} className="flex justify-between w-full border-none">
-						{row.map((col, colIndex) => (
-							<div key={colIndex} className="sm:w-[32%] w-full mb-5">
-								{col}
-							</div>
-						))}
-					</div>
-				))}
+					))}
 		</div>
 	);
 
-	const noUserMatchMessage = <p className="text-black h-[calc(100vh-7rem-94px)] flex justify-center items-center">No user match</p>;
+	const noQuizMatchMessage = <p className="text-black h-[calc(100vh-7rem-94px)] flex justify-center items-center">No Quiz match</p>;
 
 	return (
 		<>
@@ -126,7 +127,7 @@ export default function User() {
 							</button>
 						</div>
 					</div>
-					{isLoading ? loadingSpinner : quizzes !== 'none' ? userList : noUserMatchMessage}
+					{isLoading ? loadingSpinner : quizzes !== 'none' ? userList : noQuizMatchMessage}
 					{isLoading ? (
 						loadingSpinner
 					) : quizzes !== 'none' ? (
