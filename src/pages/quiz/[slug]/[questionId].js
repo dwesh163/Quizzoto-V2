6fc61574e1e723207a28e4ca827a9c94c79b06ae
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-14 16:22:59 +0200

    [update] styling for question card

--- src/pages/quiz/[slug]/[questionId].js
+++ src/pages/quiz/[slug]/[questionId].js
@@ -229,10 +229,10 @@ export default function Question() {
 						<p className="mt-4">Question not Found</p>
 					</div>
 				) : (
-					<div className="h-[calc(100vh-130px)] max-w-6xl mt-[5rem] pb-5 mx-auto md:px-6 lg:px-8 bg-white md:bg-[#fcfcfc]">
+					<div className="h-[calc(100vh-130px)] max-w-6xl mt-[5rem] sm:mt-24 pb-5 mx-auto md:px-6 lg:px-8 bg-white md:bg-[#fcfcfc]">
 						<Menu title={quiz.title} />
 						<div className="flex items-center justify-center w-full h-full sm:px-5 md:px-24 gap-5">
-							<div className="relative w-full p-5 bg-white h-[30rem] md:bg-card-texture bg-no-repeat bg-top md:rounded-2xl md:shadow-xl ">
+							<div className="relative w-full p-5 bg-white h-[30rem] md:bg-card-texture bg-no-repeat bg-top md:rounded-2xl md:shadow-xl">
 								<div className="flex justify-between mb-8">
 									<button
 										className="w-24 text-left"
