46fb14725a98f0e2ca45703d0d45168096becb31
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-14 08:57:03 +0200

    [update] code to improve performance and readability

--- src/pages/quiz/[slug]/index.js
+++ src/pages/quiz/[slug]/index.js
@@ -45,110 +45,53 @@ export default function Quiz() {
 						{quiz == '404' ? <p className="mt-4">Quiz not Found</p> : <></>}
 					</div>
 				) : (
-					<div className="h-[calc(100vh-130px)] max-w-6xl mt-24 pb-5 mx-auto md:px-6 lg:px-8 bg-white md:bg-[#fcfcfc]">
+					<div className="h-[calc(100vh-130px)] max-w-6xl mt-[5rem] sm:mt-24 pb-5 mx-auto md:px-6 lg:px-8 bg-white md:bg-[#fcfcfc]">
 						<Menu title={quiz.title} />
 						<div className="flex flex-col justify-between items-center md:flex-row gap-6 h-full">
-							<div className="bg-white md:bg-card-texture h-full bg-no-repeat bg-top p-5 md:rounded-xl md:shadow-xl w-full">
-								<h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-800 md:text-4xl lg:text-5xl ">{quiz.title}</h1>
-								<div className="flex justify-evenly mt-6 py-6 border border-neutral border-r-0 border-b-0 border-l-0">
-									<div className="text-center">
-										<h3 className="font-bold text-secondary">{quiz.info.date}</h3>
-										<p className="text-xs text-text tracking-widest">Date</p>
+							<div className="bg-white md:bg-card-texture h-full bg-no-repeat bg-top p-5 ">
+								<div class="max-w-4xl mx-auto grid grid-cols-1 lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
+									<div class="relative p-3 col-start-1 row-start-1 flex flex-col-reverse rounded-lg bg-gradient-to-t from-black/75 via-black/0 sm:bg-none sm:row-start-2 sm:p-0 lg:row-start-1">
+										<h1 class="mt-1 text-lg font-semibold z-20 text-white sm:text-slate-900 md:text-2xl">{quiz.title}</h1>
+										<p class="text-sm leading-4 font-medium z-20 text-white sm:text-slate-500">Quiz</p>
 									</div>
-									<div className="text-center">
-										<h3 className="font-bold text-secondary">{quiz.info.timeLimit}</h3>
-										<p className="text-xs text-text tracking-widest">Time Limit</p>
+									<div class="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
+										<div class="relative w-full h-60 sm:h-52 sm:col-span-2 lg:col-span-full">
+											<div class="absolute rounded-lg inset-0 overflow-hidden">
+												<img src={quiz.image} alt={quiz.title} class="w-full h-full object-cover rounded-lg" loading="lazy" />
+												<div class="rounded-lg absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50 sm:hidden"></div>
+											</div>
+										</div>
 									</div>
-									<div className="text-center">
-										<h3 className="font-bold text-secondary">{quiz.info.points}</h3>
-										<p className="text-xs text-text tracking-widest">Points</p>
+									<div class="mt-4 text-xs font-medium flex items-center justify-between row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
+										<span>
+											{quiz.rating != 0 && (
+												<div className="flex items-center">
+													{Array.from({ length: 5 }).map((_, ratingIndex) =>
+														ratingIndex + 1 > Math.round(quiz.rating) ? (
+															<svg key={ratingIndex + '-rating'} className="w-4 h-4 ms-1 text-gray-300 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+																<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+															</svg>
+														) : (
+															<svg key={ratingIndex + '-rating'} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+																<path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+															</svg>
+														)
+													)}
+												</div>
+											)}
+										</span>
+										<span class="mx-3 text-slate-800 items-center flex gap-2">
+											<span>{quiz.info.length} Questions</span>
+											<span>·</span>
+											<span>{quiz.info.points} Points</span>
+										</span>
 									</div>
-									<div className="text-center">
-										<h3 className="font-bold text-secondary">{quiz.info.length}</h3>
-										<p className="text-xs text-text tracking-widest">Questions</p>
-									</div>
-								</div>
-								<button onClick={() => router.push(quiz.slug + '/1')} className="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-sky-500 dark:hover:bg-sky-600 focus:outline-none dark:focus:ring-sky-800">
-									START
-								</button>
-							</div>
-							<div className="md:w-[30rem] h-full flex flex-col gap-6">
-								<div onClick={() => router.push('/user/' + quiz.user.username)} className="flex flex-col items-center justify-center pt-6 sm:pt-0  w-full h-96 bg-white md:bg-card-texture bg-no-repeat bg-top md:rounded-xl md:shadow-xl cursor-pointer">
-									<img src={quiz.user.image} alt="Profile Picture" className="rounded-full border-4 bg-white border-white w-32" />
-									<h1 className="font-bold font-sans text-secondary mt-2">{quiz.user.name ? quiz.user.name : quiz.user.username}</h1>
-									<h2 className="text-text font-sans text-sm text-gray-500">{quiz.user.name ? '@' + quiz.user.username : ''}</h2>
-								</div>
-								<div className="flex flex-col h-full items-center pt-6 sm:pt-8 bg-white md:bg-card-texture bg-no-repeat bg-top md:rounded-xl md:shadow-xl w-full">
-									<div className="flex items-center justify-between mb-4 w-full px-6">
-										<h5 className="text-xl font-bold leading-none text-gray-900">Latest answers</h5>
-										<button onClick={() => router.push(router.asPath + '/history')} className="text-sm font-medium text-sky-600 hover:underline">
-											View all
+									<div class="mt-4 col-start-1 row-start-3 self-center sm:mt-0 sm:col-start-2 sm:row-start-2 sm:row-span-2 lg:mt-6 lg:col-start-1 lg:row-start-3 lg:row-end-4">
+										<button onClick={() => router.push(quiz.slug + '/1')} type="button" class="text-white w-full sm:w-fit bg-sky-700 hover:bg-sky-800 focus:ring-1 focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-5 dark:bg-sky-500 dark:hover:bg-sky-600 focus:outline-none">
+											Start
 										</button>
 									</div>
-									<div className="flow-root">
-										<ul role="list" className="divide-y divide-gray-200">
-											<li className="py-3 sm:py-4">
-												<div className="flex items-center">
-													<div className="flex-shrink-0">
-														<img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-1.jpg" alt="Neil image" />
-													</div>
-													<div className="flex-1 min-w-0 ms-4">
-														<p className="text-sm font-medium text-gray-900 truncate">Neil Sims</p>
-														<p className="text-sm text-gray-500 truncate">email@windster.com</p>
-													</div>
-													<div className="inline-flex items-center text-base font-semibold text-gray-900">$320</div>
-												</div>
-											</li>
-											<li className="py-3 sm:py-4">
-												<div className="flex items-center">
-													<div className="flex-shrink-0">
-														<img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image" />
-													</div>
-													<div className="flex-1 min-w-0 ms-4">
-														<p className="text-sm font-medium text-gray-900 truncate">Bonnie Green</p>
-														<p className="text-sm text-gray-500 truncate">email@windster.com</p>
-													</div>
-													<div className="inline-flex items-center text-base font-semibold text-gray-900">$3467</div>
-												</div>
-											</li>
-											<li className="py-3 sm:py-4">
-												<div className="flex items-center">
-													<div className="flex-shrink-0">
-														<img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-2.jpg" alt="Michael image" />
-													</div>
-													<div className="flex-1 min-w-0 ms-4">
-														<p className="text-sm font-medium text-gray-900 truncate">Michael Gough</p>
-														<p className="text-sm text-gray-500 truncate">email@windster.com</p>
-													</div>
-													<div className="inline-flex items-center text-base font-semibold text-gray-900">$67</div>
-												</div>
-											</li>
-											<li className="py-3 sm:py-4">
-												<div className="flex items-center">
-													<div className="flex-shrink-0">
-														<img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-4.jpg" alt="Lana image" />
-													</div>
-													<div className="flex-1 min-w-0 ms-4">
-														<p className="text-sm font-medium text-gray-900 truncate">Lana Byrd</p>
-														<p className="text-sm text-gray-500 truncate">email@windster.com</p>
-													</div>
-													<div className="inline-flex items-center text-base font-semibold text-gray-900">$367</div>
-												</div>
-											</li>
-											<li className="pt-3 pb-0 sm:pt-4">
-												<div className="flex items-center">
-													<div className="flex-shrink-0">
-														<img className="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-5.jpg" alt="Thomas image" />
-													</div>
-													<div className="flex-1 min-w-0 ms-4">
-														<p className="text-sm font-medium text-gray-900 truncate">Thomes Lean</p>
-														<p className="text-sm text-gray-500 truncate">email@windster.com</p>
-													</div>
-													<div className="inline-flex items-center text-base font-semibold text-gray-900">$2367</div>
-												</div>
-											</li>
-										</ul>
-									</div>
+									<p class="mt-4 text-sm leading-6 col-start-1 sm:col-span-2 lg:mt-6 lg:row-start-4 lg:col-span-1 dark:text-slate-400">{quiz.description}</p>
 								</div>
 							</div>
 						</div>
