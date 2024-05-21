f55e52469818a640ebfadba4243ca7cf2f898980
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-21 12:26:01 +0200

    [update] homepage layout and add Quizzoto introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,7 +25,29 @@ export default function Home() {
 			<main>
 				<Header />
 				<div className="max-w-6xl mt-[5rem] sm:mt-24 pb-5 mx-auto md:px-6 lg:px-8 bg-white md:bg-[#fcfcfc]">
-					<h3 class="px-5 text-3xl text-gray-800 font-bold">Popular quizzes</h3>
+					<div class="text-center py-16 md:py-24">
+						<h1 class="text-5xl px-5 md:text-7xl font-extrabold leading-tighter tracking-tighter mb-4 aos-init aos-animate" data-aos="zoom-y-out">
+							Create and play on <span class="bg-clip-text text-transparent bg-gradient-to-r from-sky-500 to-teal-400">QUIZZOTO</span>
+						</h1>
+						<div class="max-w-3xl px-5 mx-auto">
+							<p class="sm:text-xl text-xs text-gray-600 mb-8 aos-init aos-animate" data-aos="zoom-y-out" data-aos-delay="150">
+								Quizzoto is a website dedicated to the creation and participation in online quizzes.
+							</p>
+							<div class="max-w-xs mx-auto sm:max-w-none select-none flex justify-center aos-init aos-animate gap-4" data-aos="zoom-y-out" data-aos-delay="300">
+								<div>
+									<a onClick={() => router.push('/quiz')} class="btn cursor-pointer px-6 py-3 rounded-lg text-white bg-sky-500 hover:bg-sky-600 w-full mb-4 sm:w-auto sm:mb-0">
+										Start quizz
+									</a>
+								</div>
+								<div>
+									<a onClick={() => router.push('/what')} class="btn cursor-pointer px-6 py-3 rounded-lg text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4">
+										Learn more
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<h3 class="px-5 text-3xl text-gray-800 font-bold text-center mb-2">Popular quizzes</h3>
 					<div className="px-5 py-2 flex lg:flex-nowrap flex-wrap gap-4 lg:gap-2 overflow-scroll">
 						{quizzes.map((quiz) => (
 							<div onClick={() => router.push('/quiz/' + quiz.slug)} className="cursor-pointer grid grid-cols-1 w-full lg:w-1/3">
@@ -47,7 +69,7 @@ export default function Home() {
 									<div className="relative w-full h-52 lg:h-52">
 										{quiz.image && (
 											<div className="absolute rounded-lg inset-0 overflow-hidden">
-												<img src={quiz.image} alt={quiz.title} className="w-full h-full object-cover rounded-lg" loading="lazy" />
+												<img src={quiz.image} className="w-full h-full object-cover rounded-lg" loading="lazy" />
 												<div className="rounded-lg absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"></div>
 											</div>
 										)}
