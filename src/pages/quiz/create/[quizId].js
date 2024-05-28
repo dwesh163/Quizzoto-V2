fd7088623c8ed45167d24908dbca9b4b0e720112
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-05-28 10:31:06 +0200

    [update] answer limit in quiz creation

--- src/pages/quiz/create/[quizId].js
+++ src/pages/quiz/create/[quizId].js
@@ -500,7 +500,7 @@ export default function Page() {
 														</button>
 													</div>
 												))}
-												{question.answers.length < 4 && (
+												{question.answers.length < 8 && (
 													<button
 														className="text-white bg-sky-700 hover:bg-sky-800 focus:ring-1 focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-5 dark:bg-sky-500 dark:hover:bg-sky-600 focus:outline-none"
 														onClick={(event) => {
