375ba937113c756ee47a6409a954144d40356ae9
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-06-03 10:31:11 +0200

    [add] test for quizzes

--- src/pages/room/[roomId]/index.js
+++ src/pages/room/[roomId]/index.js
@@ -131,13 +131,19 @@ function Settings({ parameters }) {
 						<div class="flex justify-between w-full items-center">
 							<h5 class="text-xl font-bold leading-none text-gray-900 pe-1">User</h5>
 							<p
-								className="cursor-pointer select-none"
-								onClick={() => {
-									console.log(url);
+								className="cursor-pointer select-none text-blue-600 hover:underline"
+								onClick={(e) => {
+									const element = e.target;
+									const originalText = element.innerText;
+
 									navigator.clipboard
 										.writeText(url)
 										.then(() => {
 											console.log('URL copied to clipboard:', url);
+											element.innerText = 'Copied';
+											setTimeout(() => {
+												element.innerText = originalText;
+											}, 3000);
 										})
 										.catch((error) => {
 											console.error('Failed to copy URL to clipboard:', error);
@@ -204,15 +210,17 @@ function Settings({ parameters }) {
 										)}
 									</div>
 
-									<div className="flex truncate">
-										<button onClick={() => setAuthorized(authorized.filter((a) => a.id !== user.id))} class="flex gap-1 bg-red-300 text-red-600 text-sm font-medium me-2 pl-1.5 px-2.5 py-0.5 rounded cursor-pointer truncate">
-											<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mt-0.5" viewBox="0 0 16 16">
-												<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-												<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-											</svg>
-											Remove
-										</button>
-									</div>
+									{parameters.admin !== user.id && (
+										<div className="flex truncate">
+											<button onClick={() => setAuthorized(authorized.filter((a) => a.id !== user.id))} class="flex gap-1 bg-red-300 text-red-600 text-sm font-medium me-2 pl-1.5 px-2.5 py-0.5 rounded cursor-pointer truncate">
+												<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="mt-0.5" viewBox="0 0 16 16">
+													<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+													<path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+												</svg>
+												Remove
+											</button>
+										</div>
+									)}
 								</div>
 							))}
 						</div>
@@ -501,7 +509,7 @@ function Quizzes({ oldQuizzes }) {
 									</div>
 								))}
 						</>
-					)}
+					)} */}
 					{filteredQuizzes.length > 0 && (
 						<>
 							{filteredQuizzes
@@ -514,7 +522,7 @@ function Quizzes({ oldQuizzes }) {
 									</div>
 								))}
 						</>
-					)} */}
+					)}
 				</div>
 			) : (
 				<div className="flex justify-center items-center w-full h-[70vh]"></div>
@@ -579,21 +587,47 @@ export default function Rooms() {
 								<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
 							</svg>
 						) : (
-							<p className="mt-4">Rooms not Found</p>
+							<>
+								<p className="mt-4">Rooms not Found</p>
+								<p>Try to signIn</p>
+							</>
 						)}
 					</div>
 				) : (
 					<div className="flex mt-20 md:bg-[#fcfcfc] bg-white flex-col max-w-6xl mx-auto items-center justify-between md:px-6 px-4 lg:px-8">
-						<a target="_blank" href={url}>
-							{url}
-						</a>
+						<div className="flex justify-between w-full mt-4 mb-7">
+							<div>
+								<h3 className="text-3xl font-bold text-gray-900">Room {room?.room?.title}</h3>
+								<p className="text-gray-500">{room?.room?.comment}</p>
+							</div>
+							<div className="flex gap-2 items-center">
+								<p
+									className="cursor-pointer select-none bg-sky-500 hover:opacity-90 text-white px-4 py-2 rounded-lg h-10"
+									onClick={(e) => {
+										const element = e.target;
+										const originalText = element.innerText;
 
-						<button onClick={() => navigator.clipboard.writeText(url)} className="bg-sky-500 text-white px-4 py-2 rounded-lg mt-4">
-							COPY
-						</button>
-						<a href={'/room/' + room?.room?.id + '/qr'} target="_blank" className="bg-sky-500 text-white px-4 py-2 rounded-lg mt-4">
-							Open QR
-						</a>
+										navigator.clipboard
+											.writeText(url)
+											.then(() => {
+												console.log('URL copied to clipboard:', url);
+												element.innerText = 'Copied';
+												setTimeout(() => {
+													element.innerText = originalText;
+												}, 3000);
+											})
+											.catch((error) => {
+												console.error('Failed to copy URL to clipboard:', error);
+											});
+									}}>
+									Copy link
+								</p>
+
+								<a href={'/room/' + room?.room?.id + '/qr'} target="_blank" className="cursor-pointer select-none bg-sky-500 hover:opacity-90 text-white px-4 py-2 rounded-lg h-10">
+									Open QR
+								</a>
+							</div>
+						</div>
 
 						<div className="border-b border-gray-200 w-full">
 							<nav className="-mb-px flex gap-6 w-full" aria-label="Tabs">
