550d860422444c4b9dd45ac529d09cdfd691f02f
dwesh163 <ehouarn.duriaux@protonmail.com>
2024-06-07 11:57:41 +0200

    [add] column count state and loading skeleton component

--- src/pages/quiz/index.js
+++ src/pages/quiz/index.js
@@ -13,13 +13,12 @@ export default function User() {
 	const [limit, setLimit] = useState(1);
 	const [serverSearch, setServerSearch] = useState('');
 	const [order, setOrder] = useState('date');
+	const [columnCount, setColumnCount] = useState(3);
 
 	function fetchData() {
 		if (serverSearch == search && search != '') {
 			return;
 		}
-
-		setIsLoading(true);
 
 		fetch(`/api/quiz/?limit=${limit}&search=${search}&order=${order}`)
 			.then((response) => response.json())
@@ -55,12 +54,56 @@ export default function User() {
 		}
 	};
 
-	const loadingSpinner = (
-		<div className="flex md:bg-[#fcfcfc] bg-white flex-col max-w-6xl px-2 mx-auto items-center justify-center md:flex-row md:px-6 lg:px-8  h-[100vh]">
-			<svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin fill-sky-500 bg-opacity-90" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
-				<path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
-			</svg>
+	useEffect(() => {
+		function handleResize() {
+			setColumnCount(window.innerWidth >= 640 ? 3 : 1);
+		}
+
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	const loadingSkeleton = (
+		<div className="divide-y mt-3 px-4 divide-gray-200 w-full flex flex-wrap justify-between items-center">
+			{Array.from({ length: limit * 6 })
+				.map((_, index) => (
+					<div key={index + '-quiz-list'} className="mb-5 w-full h-full cursor-pointer rounded overflow-hidden shadow-lg">
+						<div class="w-full mb-5 h-full animate-pulse">
+							<div class="mb-5 w-full h-full cursor-pointer rounded overflow-hidden shadow-lg">
+								<div class="w-full sm:h-48 h-52 bg-gray-200"></div>
+								<div class="sm:px-6 sm:py-4 px-4 py-3">
+									<div class="flex justify-between mb-2">
+										<div class="h-6 bg-gray-200 w-3/4"></div>
+										<div class="flex items-center h-6 w-6 bg-gray-200"></div>
+									</div>
+									<div class="h-12 bg-gray-200 rounded w-full mt-1"></div>
+								</div>
+								<div class="px-6 pt-4 flex flex-wrap">
+									<span class="inline-block w-32 bg-gray-200 rounded-full h-6 px-3 mr-2 mb-2"></span>
+									<span class="inline-block w-16 bg-gray-200 rounded-full h-6 px-3 mr-2 mb-2"></span>
+									<span class="inline-block w-24 bg-gray-200 rounded-full h-6 px-3 mr-2 mb-2"></span>
+								</div>
+							</div>
+						</div>
+					</div>
+				))
+				.reduce((rows, div, index) => {
+					if (index % columnCount === 0) rows.push([]);
+					rows[rows.length - 1].push(div);
+					return rows;
+				}, [])
+				.map((row, rowIndex) => (
+					<div key={rowIndex} className="flex justify-between w-full border-none">
+						{row.map((col, colIndex) => (
+							<div key={colIndex} className="sm:w-[32%] w-full mb-5 min-h-[30rem]">
+								{col}
+							</div>
+						))}
+					</div>
+				))}
 		</div>
 	);
 
@@ -110,7 +153,7 @@ export default function User() {
 					.map((row, rowIndex) => (
 						<div key={rowIndex} className="flex justify-between w-full border-none">
 							{row.map((col, colIndex) => (
-								<div key={colIndex} className="sm:w-[32%] w-full mb-5">
+								<div key={colIndex} className="sm:w-[32%] w-full mb-5 min-h-[30rem]">
 									{col}
 								</div>
 							))}
@@ -144,10 +187,8 @@ export default function User() {
 							</button>
 						</div>
 					</div>
-					{isLoading ? loadingSpinner : quizzes !== 'none' ? userList : noQuizMatchMessage}
-					{isLoading ? (
-						loadingSpinner
-					) : quizzes !== 'none' ? (
+					{isLoading ? typeof window != undefined ? loadingSkeleton : <></> : quizzes !== 'none' ? userList : noQuizMatchMessage}
+					{!isLoading && quizzes !== 'none' ? (
 						<button
 							onClick={() => {
 								setLimit(limit + 1);
